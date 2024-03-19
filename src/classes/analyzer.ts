03dc1d350baa51259dd59b2a1ebbdf97dbbb6e88
cookieukw <65344982+cookieukw@users.noreply.github.com>
2024-03-19 19:11:37 -0300

    Correcting bug where identical words were considered different due to case sensitivity or accents

--- src/classes/analyzer.ts
+++ src/classes/analyzer.ts
@@ -3,43 +3,50 @@ import util from "./utils";
 import i18n from "./translation";
 
 interface ISynon {
-  word: string[];
-  reply: string[];
-  id: string;
+    word: string[];
+    reply: string[];
+    id: string;
 }
 
 export async function analyzer(message: string): Promise<string> {
-  const search: ISynon[] = await db.synons.toArray();
-  
-  const cleaned: string[] = util.clear(message);
+    const search: ISynon[] = (await db.synons.toArray()).map((item: ISynon) => {
+        return {
+            ...item,
+            word: item.word.map(word => {
+                return util.clear(word).join(" ");
+            })
+        };
+    });
 
-  for (const word of cleaned) {
-    const matched: ISynon | undefined = search.find((item: ISynon) =>
-      item.word.includes(word)
-    );
+    const cleaned: string[] = util.clear(message);
 
-    if (matched) {
-      const randomIndex: number = Math.floor(
-        Math.random() * matched.reply.length
-      );
-      return matched.reply[randomIndex];
+    for (const word of cleaned) {
+        const matched: ISynon | undefined = search.find((item: ISynon) =>
+            item.word.includes(word)
+        );
+
+        if (matched) {
+            const randomIndex: number = Math.floor(
+                Math.random() * matched.reply.length
+            );
+            return matched.reply[randomIndex];
+        }
     }
-  }
 
-  const matchedMessage: ISynon | undefined = search.find((item: ISynon) =>
-    item.word.includes(cleaned.join(" "))
-  );
-
-  if (matchedMessage) {
-    const randomIndex: number = Math.floor(
-      Math.random() * matchedMessage.reply.length
+    const matchedMessage: ISynon | undefined = search.find((item: ISynon) =>
+        item.word.includes(cleaned.join(" "))
     );
-    return matchedMessage.reply[randomIndex];
-  }
-  const responsePromise = await import(
-    `../response/response_${i18n.language}.json`
-  );
-  const response = await responsePromise.default;
-  const randomIndex: number = Math.floor(Math.random() * response.length);
-  return response[randomIndex];
+
+    if (matchedMessage) {
+        const randomIndex: number = Math.floor(
+            Math.random() * matchedMessage.reply.length
+        );
+        return matchedMessage.reply[randomIndex];
+    }
+    const responsePromise = await import(
+        `../response/response_${i18n.language}.json`
+    );
+    const response = await responsePromise.default;
+    const randomIndex: number = Math.floor(Math.random() * response.length);
+    return response[randomIndex];
 }
