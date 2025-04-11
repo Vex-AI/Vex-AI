ff8549f1399e9fe72a6e8c9d576f2a723e624ecd
cookieukw <cookieukw@users.noreply.github.com>
2025-04-10 22:53:26 -0300

    #

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-    appId: "com.new",
+    appId: "com.cookieukw.vex",
     appName: "Vex",
     webDir: "dist",
     android: {
