af25de3674d7966943ba1ac649fca08f7470404c
cookieukw <cookieukw@users.noreply.github.com>
2025-11-14 19:50:04 -0300

    #

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import A from "vite-plugin-pages";
-
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), A({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    tailwindcss(),
+    A({
       dirs: "./app",
       extensions: ["tsx"],
-    }),], root: './'
+    }),
+  ],
+  root: "./",
 });
